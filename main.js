948b6228d25cb105719ae4a2e695f8027266cafa
Daniela <danscarballo@gmail.com>
2023-06-17 20:31:29 -0300

    Muestra sus datos correctamente

--- main.js
+++ main.js
@@ -1,39 +1,42 @@
-const listaPokemon = document.getElementById('listaPokemon');
-const URL = 'https://pokeapi.co/api/v2/';
-
+const listaPokemon = document.querySelector("#lista-pokemon");
+const URL = "https://pokeapi.co/api/v2/";
 
 for (let i = 1; i <= 151; i++) {
-    fetch(`${URL}pokemon/${i}`)
-        .then(response => response.json())
-        .then(data => console.log(data))
-        .catch(error => console.log(error));
+  fetch(`${URL}pokemon/${i}`)
+    .then((response) => response.json())
+    .then((data) => mostrarPokemon(data))
+    .catch((error) => console.log(error));
 }
 
+const mostrarPokemon = (data) => {
+  const tipos = data.types.map((tipo) => 
+    
+      `<p class="tipo">${tipo.type.name}</p>`
+    )
+  .join("");
 
-
-
-/*
-<div class="pokemon">
-            <p class="pokemon-id-back">#03</p>
-            <div class="pokemon-imagen">
-              <img
-                src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/4.svg"
-                alt="Charmander"
-              />
-            </div>
-            <div class="pokemon-info">
-              <div class="nombre-contenedor">
-                <p class="pokemon-id">#03</p>
-                <h2 class="pokemon-nombre">Charmander</h2>
-              </div>
-              <div class="pokemon-tipos">
-                <p class="tipo">Fuego</p>
-                <p class="tipo">Fuego</p>
-              </div>
-              <div class="pokemon-stats">
-                <p class="stats">4m</p>
-                <p class="stats">60kg</p>
-              </div>
-            </div>
-          </div>
-*/
+  const div = document.createElement("div");
+  div.classList.add("pokemon");
+  div.innerHTML = `
+    <p class="pokemon-id-back">#${data.id}</p>
+    <div class="pokemon-imagen">
+      <img
+        src="${data.sprites.other.dream_world.front_default}"
+        alt="${data.name}"
+      />
+    </div>
+    <div class="pokemon-info">
+      <div class="nombre-contenedor">
+        <p class="pokemon-id">#${data.id}</p>
+        <h2 class="pokemon-nombre">${data.name}</h2>
+      </div>
+      <div class="pokemon-tipos">
+        ${tipos}
+      </div>
+      <div class="pokemon-stats">
+        <p class="stats">${data.height}M</p>
+        <p class="stats">${data.weight}KG</p>
+      </div>
+    </div>`;
+  listaPokemon.appendChild(div);
+};
