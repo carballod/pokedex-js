50512d88b04c522b540103ad0b73593901e22767
Daniela <danscarballo@gmail.com>
2023-06-26 08:02:55 -0300

    Test en Cypress

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -4,6 +4,18 @@ describe("Pokedex Test", () => {
     cy.visit("/index.html");
   });
 
+  it("Muestra los primeros 151 Pokémon", () => {
+    cy.get(".pokemon").should("have.length", 151);
+  });
   
+  it("Carga los elementos de la página correctamente", () => {
+    cy.title().should("eq", "Pokedex");
+  
+    cy.get("#pokemon-logo").should("be.visible");
+    cy.get("#pokemon-logo").should("have.attr", "src", "./img/logo.png");
+  
+    cy.get("#buscar-pokemon").should("be.visible");
+    cy.get("#lista-pokemon").should("be.visible");
+  });
 
 })
