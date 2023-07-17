b0b4abbbec80fabf01a0999f7ccc84c0c92e6237
Daniela <danscarballo@gmail.com>
2023-07-16 22:40:43 -0300

    Agrega la entidad pokemon

--- src/mapeadores/pokemonMap.js
+++ src/mapeadores/pokemonMap.js
@@ -1,20 +1,36 @@
+import Pokemon from "../entidades/pokemon.js";
+
 const listaPokemon = document.querySelector("#lista-pokemon");
 
 export const mostrarPokemon = (data) => {
+  const pokemon = new Pokemon(
+    data.id,
+    data.name,
+    data.sprites.other.dream_world.front_default,
+    data.types.map((tipo) => tipo.type.name),
+    data.abilities.map((habilidad) => habilidad.ability.name),
+    data.moves.map((movimiento) => ({
+      movimiento: movimiento.move.name,
+      versiones: movimiento.version_group_details.map(
+        (version) => version.version_group.name
+      ),
+    }))
+  );
+
   const div = document.createElement("div");
   div.classList.add("pokemon");
   div.innerHTML = `
-    <p class="pokemon-id-back">#${data.id}</p>
+    <p class="pokemon-id-back">#${pokemon.id}</p>
     <div class="pokemon-imagen">
       <img
-        src="${data.sprites.other.dream_world.front_default}"
-        alt="${data.name}"
+        src="${pokemon.foto}"
+        alt="${pokemon.nombre}"
       />
     </div>
     <div class="pokemon-info">
       <div class="nombre-contenedor">
-        <p class="pokemon-id">#${data.id}</p>
-        <h2 class="pokemon-nombre">${data.name}</h2>
+        <p class="pokemon-id">#${pokemon.id}</p>
+        <h2 class="pokemon-nombre">${pokemon.nombre}</h2>
       </div>
     </div>`;
   listaPokemon.appendChild(div);
